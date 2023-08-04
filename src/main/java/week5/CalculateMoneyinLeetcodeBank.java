1430a6413deb48a7692f6289138ca5d3a86ea18b
arun-qa <mailtoarun1911@gmail.com>
2023-08-04 05:31:57 +0530

    calculate money in leet code solved

---
+++ src/main/java/week5/CalculateMoneyinLeetcodeBank.java
@@ -0,0 +1,50 @@
+package week5;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class CalculateMoneyinLeetcodeBank {
+	
+	
+	@Test
+	public void test1()
+	{
+		Assert.assertEquals(10, numberofdays(4));
+	}
+	
+	@Test
+	public void test2()
+	{
+		Assert.assertEquals(37, numberofdays(10));
+	}
+	
+	@Test
+	public void test3()
+	{
+		Assert.assertEquals(96, numberofdays(20));
+	}
+	
+	
+
+	public int numberofdays(int n) {
+		
+		
+		int totalmoney = 0;
+		int dailymoney = 1;
+		for (int i = 1; i <= n; i++) 
+		{
+			totalmoney = totalmoney + dailymoney++;
+			
+			if(i%7 == 0) 
+			{
+				
+				dailymoney = dailymoney - 6;
+		}
+		
+	}
+		return totalmoney;
+	}
+		
+
+}
