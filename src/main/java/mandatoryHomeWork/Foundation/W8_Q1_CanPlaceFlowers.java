50e4643bf75962b0b948503d655532174be4c13e
arun-qa <mailtoarun1911@gmail.com>
2023-08-23 16:42:14 +0530

    daily challenge solved

---
+++ src/main/java/mandatoryHomeWork/Foundation/W8_Q1_CanPlaceFlowers.java
@@ -0,0 +1,43 @@
+package mandatoryHomeWork.Foundation;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+public class W8_Q1_CanPlaceFlowers {
+	
+	@Test
+	public void test1()
+	{														 
+		Assert.assertEquals(true, canPlaceFlowers(new int[] {1,0,0,0,1},1));
+	}
+	
+	@Test
+	public void test2()
+	{														 
+		Assert.assertEquals(true, canPlaceFlowers(new int[] {1,0,0,0,1},1));
+	}
+	
+	public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            
+            if (flowerbed[i] == 0) {
+                
+                boolean emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean emptyRightPlot = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
+                
+                
+                if (emptyLeftPlot && emptyRightPlot) {
+                    flowerbed[i] = 1;
+                    count++;
+                    if (count >= n) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return count >= n;
+    }
+}
+
+	
