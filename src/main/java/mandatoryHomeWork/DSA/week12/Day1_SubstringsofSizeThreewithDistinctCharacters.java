664a2f9ba6251aaec07e2c3b6a90c9143cfdb180
arun-qa <mailtoarun1911@gmail.com>
2023-09-20 00:30:44 +0530

    Solved by sliding window

---
+++ src/main/java/mandatoryHomeWork/DSA/week12/Day1_SubstringsofSizeThreewithDistinctCharacters.java
@@ -0,0 +1,27 @@
+package mandatoryHomeWork.DSA.week12;
+
+public class Day1_SubstringsofSizeThreewithDistinctCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * Example 1:
+		 * 
+		 * Input: s = "xyzzaz" Output: 1 Explanation: There are 4 substrings of size 3:
+		 * "xyz", "yzz", "zza", and "zaz". The only good substring of length 3 is "xyz".
+		 */
+		
+		String s= "xyzzaz";
+		
+		char[] charArray = s.toCharArray();
+		
+		for(int i=0; i<charArray.length-3; i++)
+		{
+			
+				}
+			}
+		
+	
+
+}
