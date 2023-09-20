a9d66316ecfecf3357a3a2d98a15b17882159ab2
arun-qa <mailtoarun1911@gmail.com>
2023-09-20 22:37:55 +0530

    solved by sliding window

--- src/main/java/mandatoryHomeWork/DSA/week12/Day1_SubstringsofSizeThreewithDistinctCharacters.java
+++ src/main/java/mandatoryHomeWork/DSA/week12/Day1_SubstringsofSizeThreewithDistinctCharacters.java
@@ -1,27 +1,64 @@
 package mandatoryHomeWork.DSA.week12;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
 public class Day1_SubstringsofSizeThreewithDistinctCharacters {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		/*
-		 * Example 1:
-		 * 
-		 * Input: s = "xyzzaz" Output: 1 Explanation: There are 4 substrings of size 3:
-		 * "xyz", "yzz", "zza", and "zaz". The only good substring of length 3 is "xyz".
-		 */
-		
-		String s= "xyzzaz";
-		
-		char[] charArray = s.toCharArray();
-		
-		for(int i=0; i<charArray.length-3; i++)
-		{
-			
-				}
-			}
-		
+	/*
+	 * https://leetcode.com/problems/substrings-of-size-three-with-distinct-
+	 * characters/
+	 * 
+	 * Pseudocode
+	 * 
+	 * Get an input as String and output should be integer initiate one int variable
+	 * as 0 iterate for loop till s.length()
+	 * 
+	 * 
+	 * 
+	 * Time Complexity --> O(n)
+	 */
 	
+	@Test
+	public void test1()
+	{
+		Assert.assertEquals(1, countGoodSubstrings("xyzzaz"));
+	}
+	
+	
+	@Test
+	public void test2()
+	{
+		Assert.assertEquals(4, countGoodSubstrings("aababcabc"));
+	}
+
+	public int countGoodSubstrings(String s) {
+
+		int startindex = 0;
+		int count = 0;
+
+		if (s.length() < 3)
+			return 0;
+
+		Set<Character> set = new HashSet<Character>();
+
+		for (int i = 0; i < s.length(); i++) {
+
+			while (!set.add(s.charAt(i)))
+				set.remove(s.charAt(startindex++));
+			if (set.size() == 3) {
+				count++;
+				set.remove(s.charAt(startindex++));
+			}
+
+		}
+
+		return count;
+
+	}
 
 }
