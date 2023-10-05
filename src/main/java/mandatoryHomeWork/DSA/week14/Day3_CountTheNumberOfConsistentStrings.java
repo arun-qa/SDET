6ed396e3c68a0e96672f5263975d42c5b89ca778
arun-qa <mailtoarun1911@gmail.com>
2023-10-05 21:11:14 +0530

    Solved using Set

---
+++ src/main/java/mandatoryHomeWork/DSA/week14/Day3_CountTheNumberOfConsistentStrings.java
@@ -0,0 +1,54 @@
+package mandatoryHomeWork.DSA.week14;
+
+import java.util.HashSet;
+
+public class Day3_CountTheNumberOfConsistentStrings {
+
+	/*
+	 * https://leetcode.com/problems/count-the-number-of-consistent-strings/
+	 * description/
+	 * 
+	 * Pseudocode
+	 * Get two inputs one as String and another one as String[], output as int
+	 * store the each character of String in Set
+	 * create another for loop & get each word
+	 * Iterate another for loop & get the each character from each word
+	 * Check if the character not found in Set, if so set the flag as false & break the statement
+	 * increment the count & return the count
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String allowed = "abc";
+		String[] words = new String[] {"a","b","c","ab","ac","bc","abc"};
+		//boolean flag = true;
+		int count = 0;
+		
+		HashSet<Character> set = new  HashSet<>();
+		
+		for (Character ch : allowed.toCharArray()) {
+			
+			set.add(ch);
+			
+		}
+		for (String eachword : words) {
+			boolean flag = true;
+			
+			for(Character ch1 : eachword.toCharArray())
+			{
+				if(!set.contains(ch1))
+						{
+					flag = false;
+					break;
+						}
+			}
+			if(flag==true)
+				count++;
+			
+		}
+		System.out.println(count);
+
+	}
+
+}
---
+++ src/main/java/mandatoryHomeWork/DSA/week14/Day3_SumOfUniqueElements.java
@@ -0,0 +1,50 @@
+package mandatoryHomeWork.DSA.week14;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Day3_SumOfUniqueElements {
+
+	/*
+	 * https://leetcode.com/problems/sum-of-unique-elements/description/
+	 * 
+	 * Pseudocode Get input as int[] and output as int Sort the Array Compare it the
+	 * very next element If the element is equal then add it to the Set Else check
+	 * if it doesn't contains in the Set Count it Since we are missing the Last
+	 * element verify if it not there in Set then add it for Count
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] nums = new int[] { 1, 2, 3, 3 };
+
+		Arrays.sort(nums);
+
+		System.out.println(Arrays.toString(nums));
+
+		HashSet<Integer> set = new HashSet<>();
+
+		int count = 0;
+
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == nums[i + 1]) {
+				set.add(nums[i]);
+			} else {
+				if (!set.contains(nums[i])) {
+					count = count + nums[i];
+				}
+
+			}
+
+		}
+		if (!set.contains(nums[nums.length - 1])) {
+			count = count + nums[nums.length - 1];
+		}
+
+		System.out.println(count);
+
+	}
+
+}
