e57aa8cdb8ccb4d20f9a0bbe9644aed90c693fb4
arun-qa <mailtoarun1911@gmail.com>
2023-10-14 19:22:04 +0530

    solved by linked list

--- src/main/java/mandatoryHomeWork/DSA/week15/Day3_RemoveDulplicates.java
+++ src/main/java/mandatoryHomeWork/DSA/week15/Day3_RemoveDulplicates.java
@@ -1,5 +1,31 @@
 package mandatoryHomeWork.DSA.week15;
 
 public class Day3_RemoveDulplicates {
+	
+	//https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+	//Pseudocode
+	// Take the copy of head as temp
+	// Iterate temp if temp & temp.next!=null
+	// check the temp.val == temp.next.val
+	// return head
+
+	public ListNode deleteDuplicates(ListNode head) {
+
+		ListNode temp = head;
+
+		while (temp != null && temp.next != null) {
+			if (temp.val == temp.next.val) {
+				temp.next = temp.next.next;
+			}
+
+			else {
+				temp = temp.next;
+			}
+
+		}
+
+		return head;
+
+	}
 
 }
